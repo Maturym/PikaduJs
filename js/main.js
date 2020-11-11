9bb03fc05b345d5582300487ab2b03fbcbfbaad0
Elizabeth <whosingsin@gmail.com>
2020-11-11 18:58:59 +0300

    day 2

--- js/main.js
+++ js/main.js
@@ -14,7 +14,10 @@ const login = document.querySelector('.login'),
   loginForm = document.querySelector('.login-form'),
   emailInput = document.querySelector('.login-email'),
   passwordInput = document.querySelector('.login-password'),
-  loginSignup = document.querySelector('.login-signup');
+  loginSignup = document.querySelector('.login-signup')
+  userBlock = document.querySelector('.user'),
+  userName = document.querySelector('.user-name'),
+  authorName = document.querySelectorAll('.author-username');
 
 const listUsers = [
   {
@@ -30,3 +33,64 @@ const listUsers = [
     displayName: 'KateNotKill'
   }
 ];
+
+const setUsers = {
+  user: null,
+
+  logIn(email, password, handler) {
+    const user = this.getUser(email);
+    if (user && user.password === password) {
+      this.authorizedUser(user);
+      handler()
+    } else {
+      alert('Пользователь не найден')
+    }
+  },
+  logOut() {
+
+  },
+  signUp(email, password, handler) {
+    if(!this.getUser(email)){
+
+      const userName = email.split('@')[0];
+
+      const user = { email, password, displayName: userName };
+
+      listUsers.push(user);
+      this.authorizedUser(user);
+      handler();
+
+    } else {
+      alert('Это имя пользователя уже занято!')
+    }
+  },
+  getUser(email) {
+    return listUsers.find((item) => item.email === email);
+  },
+  authorizedUser(user) {
+    this.user = user;
+  } 
+};
+
+const toggleDOMAuth = () => {
+  const user = setUsers.user;
+  
+  if (user) {
+    login.style.display = 'none';
+    loginForm.style.display = 'none';
+    userBlock.style.display = 'flex';
+    userName.textContent = `${user.displayName}`;
+
+    authorName.forEach((item) => item.textContent = `${user.displayName}`);
+  }
+};
+
+loginForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  setUsers.logIn(emailInput.value, passwordInput.value, toggleDOMAuth);
+});
+
+loginSignup.addEventListener('click', (e) => {
+  e.preventDefault();
+  setUsers.signUp(emailInput.value, passwordInput.value, toggleDOMAuth);
+})
