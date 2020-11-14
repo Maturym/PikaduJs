2f880d9c20c9f5821b3c85759e1d725d16ccc8ec
Elizabeth <whosingsin@gmail.com>
2020-11-14 13:14:56 +0300

    filter by author

--- js/main.js
+++ js/main.js
@@ -118,21 +118,6 @@ const setUsers = {
   } 
 };
 
-// const filterArray = () => {
-
-
-//   const filteredArray = [];
-
-//   setPosts.allPosts.forEach(item => {
-//     if (item.tags.find(tag => tag === target)){
-//       filteredArray.push(item);
-//     }
-//   })
-//   console.log(filteredArray);
-//   // // return filteredArray;
-
-//   // // showAllPosts();
-// }
 
 const setPosts = {
   allPosts: [
@@ -191,6 +176,10 @@ const setPosts = {
 
   ],
 
+  filteredByAuthor: [
+
+  ],
+
   filteredPosts(array, handler) {
     
     array.forEach(({ title, text, like, comments, author, date, tags }) => {
@@ -200,9 +189,30 @@ const setPosts = {
         text,
         tags: tags.slice(0).map(item => item.trim()),
         author,
-        date: new Date().toLocaleString(),
-        like: 0,
-        comments: 0,
+        date,
+        like,
+        comments,
+      })
+
+    })
+    
+    if (handler) {
+      handler()
+    }
+  },
+
+  filteredPostsByAuthor(array, handler) {
+
+    array.forEach(({ title, text, like, comments, author, date, tags }) => {
+
+      this.filteredByAuthor.push({
+        title,
+        text,
+        tags: tags.slice(0).map(item => item.trim()),
+        author,
+        date,
+        like,
+        comments,
       })
 
     })
@@ -210,9 +220,8 @@ const setPosts = {
     if (handler) {
       handler()
   }
-  
-}
 
+  },
 };
 
 const toggleDOMAuth = () => {
@@ -269,7 +278,9 @@ const showAllPosts = () => {
 
   postsWrapper.innerHTML = '';
 
-    if (setPosts.filteredByTag.length === 0){
+  console.log(setPosts.filteredByAuthor);
+
+    if (setPosts.filteredByTag.length === 0 && setPosts.filteredByAuthor.length === 0){
       setPosts.allPosts.forEach(({ title, text, like, comments, author, date, tags }) => {
 
         //   // const isAuthor = setUsers.getUser(author).displayName;
@@ -329,14 +340,72 @@ const showAllPosts = () => {
           });
         });
       
-    } else {
+    } else if (setPosts.filteredByAuthor.length) {
+
+      setPosts.filteredByAuthor.forEach(({ title, text, like, comments, author: {displayName, photo}, date, tags }) => {
+        // console.log(title);
+
+        postHTML = `
+          <section class="post">
+            <div class="post-body">
+              <h2 class="post-title">${title}</h2>
+              <p class="post-text">${text}</p>
+              <div class="tags">
+              </div>
+            </div>
+            <div class="post-footer">
+              <div class="post-buttons">
+                <button class="post-button likes">
+                  <svg width="19" height="20" class="icon icon-like">
+                    <use xlink:href="img/icons.svg#like"></use>
+                  </svg>
+                  <span class="likes-counter">${like}</span>
+                </button>
+                <button class="post-button comments">
+                  <svg width="21" height="21" class="icon icon-comment">
+                    <use xlink:href="img/icons.svg#comment"></use>
+                  </svg>
+                  <span class="comments-counter">${comments}</span>
+                </button>
+                <button class="post-button save">
+                  <svg width="19" height="19" class="icon icon-save">
+                    <use xlink:href="img/icons.svg#save"></use>
+                  </svg>
+                </button>
+                <button class="post-button share">
+                  <svg width="17" height="19" class="icon icon-share">
+                    <use xlink:href="img/icons.svg#share"></use>
+                  </svg>
+                </button>
+              </div>
+              <div class="post-author">
+                <div class="author-about">
+                  <a href="#" class="author-username">${displayName}</a>
+                  <span class="post-time">${date}</span>
+                </div>
+                <a href="#" class="author-link"><img src=${photo? photo : "img/avatar.jpeg"} alt="avatar" class="author-avatar"></a>
+              </div>
+                
+              </div>
+            </div> 
+          </section>   
+        `;
+    
+        postsWrapper.insertAdjacentHTML('afterbegin', postHTML); 
+    
+        tags.forEach(tag => {
+  
+          const tagsLinks = document.querySelector('.tags')        
+    
+          tagsLinks.insertAdjacentHTML('afterbegin', `<a href="#${tag}" class="tag">#${tag}</a>`)
+        });
+      }); 
+    }
+
+    else if (setPosts.filteredByTag.length){
       
       setPosts.filteredByTag.forEach(({ title, text, like, comments, author: {displayName, photo}, date, tags }) => {
 
-
-        //   // const isAuthor = setUsers.getUser(author).displayName;
-      
-        //   // const isAvatar = setUsers.getUser(author).photo;
           postHTML = `
             <section class="post">
               <div class="post-body">
@@ -393,16 +462,16 @@ const showAllPosts = () => {
           });
         }); 
     } 
-  // postsWrapper.innerHTML = 'HERE';
   
+  setPosts.filteredByAuthor = [];
   setPosts.filteredByTag = [];
-  
-  console.log(setPosts.filteredByTag);
+
   
   tagsLinks = document.querySelectorAll('.tags')
 
   tagsLinks.forEach((item) => {
     item.addEventListener('click', e => {
+      e.preventDefault();
       target = e.target.textContent.slice(1);
 
       const filteredArray = setPosts.allPosts.filter(item => {
@@ -415,6 +484,25 @@ const showAllPosts = () => {
 
     })
   });     
+
+  const authorName = document.querySelectorAll('.author-username')
+
+  authorName.forEach((item) => {
+    item.addEventListener('click', e => {
+      e.preventDefault();
+      target = e.target.textContent;
+
+      const filteredArray = setPosts.allPosts.filter(item => {
+        if (item.author.displayName === target){
+          return item;
+        }
+      });
+
+      setPosts.filteredPostsByAuthor(filteredArray, showAllPosts);
+
+    })
+  });
+
 };
 
 
@@ -459,25 +547,6 @@ const init = () => {
     // вешаем класс на меню, когда кликнули по кнопке меню 
     menu.classList.toggle('visible');
   });
-
-  ///TEST
-  tagsLinks = document.querySelectorAll('.tags');
-
-  tagsLinks.forEach((item) => {
-    item.addEventListener('click', e => {
-      target = e.target.textContent.slice(1);
-
-      const filteredArray = setPosts.allPosts.filter(item => {
-        if(item.tags.find(tag => tag === target)){
-          return item;
-        }
-      });
-
-      setPosts.filteredPosts(filteredArray, showAllPosts);
-
-    })
-  });
-  ///ends here
 
   addPost.addEventListener('submit', e => {
     e.preventDefault();
